a7eefff8d3da09292c0846465610ec8c8632b69f
select_bonus <jburrows826@gmail.com>
2025-02-23 15:37:10 -0600

    implement player movement

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -23,7 +28,11 @@ def main():
                 return
         
         screen.fill((0, 0, 0))
-        player1.draw(screen)
+        updatable.update(dt)
+        
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
