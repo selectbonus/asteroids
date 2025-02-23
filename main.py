5d17371edcced1a9fb3995e4e67bccdf024aa293
select_bonus <jburrows826@gmail.com>
2025-02-23 14:18:13 -0600

    write circle and player classes, render player

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -17,7 +23,10 @@ def main():
                 return
         
         screen.fill((0, 0, 0))
+        player1.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
